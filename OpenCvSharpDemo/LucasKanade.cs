864646803e3e10ab49e283cd9c69f413ea4da821
Drozdov <Drozdov_VA@list.ru>
2014-09-05 03:09:17 +0400

    Initial commit

---
+++ OpenCvSharpDemo/LucasKanade.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+
+namespace OpenCvSharpDemo
+{
+    class LucasKanade
+    {
+        public int[] LucasKanadeStep(Mat img1, Mat img2, int[] t, int scale)
+        {
+            // Transform the image
+            //Translation(img2, img2t, t);
+            // Compute the gradients and summed error by comparing img1 and img2t
+            var A = new double[2, 2];
+            var b = new double[2];
+            int diff = 1;
+            errorImg = new Mat(new Size(img2.Width - 2, img2.Height - 2), MatType.CV_8SC1);
+            gradientsX = new Mat(new Size(img2.Width - 2, img2.Height - 2), MatType.CV_8SC1);
+            gradientsY = new Mat(new Size(img2.Width - 2, img2.Height - 2), MatType.CV_8SC1);
+            for (int y = diff; y < img2.Height - diff; y++)
+            { // ignore borders
+                for (int x = diff; x < img2.Width - diff; x++)
+                {
+                    // If both have full alphas, then compute and accumulate the error
+                    var e = img2.At<byte>(y, x) - img1.At<byte>(y + t[1], x + t[0]);
+                    errorImg.Set<byte>(y - diff, x - diff, (byte) (e / 4 + 63));
+                    // Accumulate the matrix entries
+                    double gx = 0.5 * (img2.At<byte>(y, x + diff) - img2.At<byte>(y, x - diff));
+                    double gy = 0.5 * (img2.At<byte>(y + diff, x) - img2.At<byte>(y - diff, x));
+
+                    gradientsX.Set<byte>(y - diff, x - diff, (byte)(gx / 2 + 63));
+                    gradientsY.Set<byte>(y - diff, x - diff, (byte)(gy / 2 + 63));
+
+                    A[0, 0] += gx * gx; A[0, 1] += gx * gy;
+                    A[1, 0] += gx * gy; A[1, 1] += gy * gy;
+                    b[0] += e * gx; b[1] += e * gy;
+                }
+            }
+            double det = 1.0 / (A[0, 0] * A[1, 1] - A[1, 0] * A[0, 1]);
+            var t0 = (int)(det * scale * (A[1, 1] * b[0] - A[1, 0] * b[1]));
+            var t1 = (int)(det * scale * (A[0, 0] * b[1] - A[0, 1] * b[0]));
+            while (!IsIntoImage(img1, t[0] + t0, t[1] + t1))
+            {
+                t0 /= 2;
+                t1 /= 2;
+            }
+            return new int[] { t[0] + t0, t[1] + t1 };
+        }
+
+        private Mat errorImg;
+        public Mat ErrorImg { get { return errorImg; } }
+
+        private Mat gradientsX;
+        public Mat GradientsX { get { return gradientsX; } }
+
+        private Mat gradientsY;
+        public Mat GradientsY { get { return gradientsY;  } }
+
+        private bool IsIntoImage(Mat img, int x, int y)
+        {
+            return x >= 0 && x < img.Width && y >= 0 && y < img.Height;
+        }
+
+        public long ImgsDiff(Mat img1, Mat img2, int[] t)
+        {
+            long res = 0;
+            for (int y = 0; y < img2.Height; y++)
+            {
+                for (int x = 0; x < img2.Width; x++)
+                {
+                    var e = img2.At<byte>(y + t[1], x + t[0]) - img1.At<byte>(y, x);
+                    res += e * e / 10000;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ OpenCvSharpDemo/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+
+// use NuGet
+namespace OpenCvSharpDemo
+{
+    class Program
+    {
+        static bool Eq(int[] arr1, int[] arr2)
+        {
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != arr2[i])
+                    return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Mat scene = new Mat("scene.png", LoadMode.GrayScale);
+            Mat obj = new Mat("obj.png", LoadMode.GrayScale);
+            var lc = new LucasKanade();
+            Console.WriteLine(scene.Width);
+            int[] t = new int[] { 131, 155 };
+            /*int[] d = lc.LucasKanadeStep(scene, obj, t);
+
+            while (!Eq(d, t))
+            {
+                Console.WriteLine(d[0] + " " + d[1]);
+                t = d;
+                d = lc.LucasKanadeStep(scene, obj, t);
+            }
+            Console.WriteLine(d[0] + " " + d[1]);*/
+
+            var oldDiff = lc.ImgsDiff(scene, obj, t);
+            for (int scale = 20; scale != 0; scale = scale * 3 / 4)
+            {
+                var p = lc.LucasKanadeStep(scene, obj, t, scale);
+                var diff = lc.ImgsDiff(scene, obj, p);
+                Console.WriteLine(oldDiff + " " + diff);
+                //if (oldDiff >= diff)
+                {
+                    t = p;
+                    oldDiff = diff;
+                }
+                //Console.WriteLine(p[0] + " " + p[1]);
+                Console.WriteLine(t[0] + " " + t[1]);
+                //Console.WriteLine("------------");
+
+                /*using (new Window("error image", lc.ErrorImg))
+                {
+                    Cv2.WaitKey();
+                }*/
+                using (new Window("error", lc.ErrorImg))
+                using (new Window("gr x", lc.GradientsX))
+                using (new Window("gr y", lc.GradientsY))
+                {
+                    Cv2.WaitKey();
+                }
+
+               
+
+            }
+
+            byte color = 0;
+            scene.Set<byte>(t[1], t[0], color);
+            scene.Set<byte>(t[1] - 1, t[0], color);
+            scene.Set<byte>(t[1] + 1, t[0], color);
+            scene.Set<byte>(t[1], t[0] - 1, color);
+            scene.Set<byte>(t[1], t[0] + 1, color);
+            scene.Set<byte>(t[1] - 2, t[0], color);
+            scene.Set<byte>(t[1] + 2, t[0], color);
+            scene.Set<byte>(t[1], t[0] - 2, color);
+            scene.Set<byte>(t[1], t[0] + 2, color);
+            scene.Set<byte>(t[1] - 3, t[0], color);
+            scene.Set<byte>(t[1] + 3, t[0], color);
+            scene.Set<byte>(t[1], t[0] - 3, color);
+            scene.Set<byte>(t[1], t[0] + 3, color);
+
+            using (new Window("src image", scene))
+            using (new Window("dst image", obj))
+            using (new Window("error image x", lc.GradientsX))
+            using (new Window("error image y", lc.GradientsY))
+            {
+                Cv2.WaitKey();
+            }
+
+            /*Mat src = new Mat("2.png", LoadMode.GrayScale);
+
+            Console.WriteLine(src.Width + " " + src.Height);
+            for (int j = 0; j < src.Height; j++) 
+            {
+                for (int i = 0; i < src.Width; i++)
+                {
+                    Console.Write(src.At<byte>(j, i) > 200 ? "1" : " ");
+                }
+                Console.WriteLine();
+            }
+
+            Mat dst = new Mat();
+
+            Cv2.Canny(src, dst, 50, 200);
+            using (new Window("src image", src))
+            using (new Window("dst image", dst))
+            {
+                Cv2.WaitKey();
+            }*/
+
+            /*IplImage src = Cv.LoadImage("lenna.png", LoadMode.GrayScale);
+            IplImage dst = Cv.CreateImage(new CvSize(src.Width, src.Height), BitDepth.U8, 1);
+            Cv.Canny(src, dst, 50, 200);
+            Cv.NamedWindow("src image");
+            Cv.ShowImage("src image", src);
+            Cv.NamedWindow("dst image");
+            Cv.ShowImage("dst image", dst);
+            Cv.WaitKey();
+            Cv.DestroyAllWindows();
+            Cv.ReleaseImage(src);
+            Cv.ReleaseImage(dst);*/
+
+        }
+    }
+}
